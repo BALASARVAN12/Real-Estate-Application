29eeb06a5149bda926d708ce8951e69590eb8691
BALASARVAN12 <91976828+BALASARVAN12@users.noreply.github.com>
2024-04-07 20:29:34 +0530

    project setup

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Hello! This is BalaSarvan</h2>
+      <Button>Hire Me!</Button>
+    </div>
+  );
+}
